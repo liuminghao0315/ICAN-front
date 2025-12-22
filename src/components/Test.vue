c02f620bf0b1be97716393a76efed65e3fbe29be
liuminghao0315 <2379355158@qq.com>
2025-12-22 19:25:43 +0800

    Revert "测试前后端连通性"
    This reverts commit 70e8a2ce321fb50d9420fbbabf5321910362f820.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,17 +1,16 @@
 <template>
     <div id="Test">
-        你好啊 ican {{ random }}
-        <button @click="getRandom()">点我获取随机数（从springboot获取）</button>
+        你好啊 ican {{ att }}
+        <button @click="add1()">点我att+1</button>
     </div>
 </template>
 
 <script setup lang="ts" name="Test">
     import {ref} from 'vue';
-    import axios from 'axios'
-    const random = ref<number>(0);
+    const att = ref<number>(0);
 
-    async function getRandom(){
-        random.value=(await axios.get("http://localhost:8080/getRandom")).data
+    function add1(){
+        att.value++;
     }
 </script>
 
